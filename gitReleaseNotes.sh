355217c017a29972b8133242a188b1e5dd07f2e4
moschen <moschen@actions-micro.com>
2014-09-02 15:46:28 +0800

    add /html argument to handle html convert, create the html file, append the html structure, append the css style call markdown2 processing the md file, and append it to the html file

--- gitReleaseNotes.sh
+++ gitReleaseNotes.sh
@@ -7,6 +7,31 @@ if [ -n "$1" ]; then
     if [ "$1" == "/all" ]; then
         Output_Filename="$Output_Filename_Prefix.md"
         Output_Header="$Output_Header_Prefix"
+    elif [ "$1" == "/html" ]; then
+        shopt -s nullglob
+        for mdfile in *.md; do
+            file=$( basename "$mdfile" )
+            html_filename=$(printf "%s.html" "$file")
+            cat > $html_filename <<EOL
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN">
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <title>$file</title>
+    <style>
+EOL
+            cat ~/bin/gitReleaseNotes/style.css >> $html_filename
+            cat >> $html_filename <<EOL
+    </style>
+    <body>
+EOL
+            markdown2 $mdfile >> $html_filename
+            cat >> $html_filename << EOL
+    </body>
+</html>
+EOL
+        done
+        exit
     else
         Output_TAG=$1
         Output_Filename=$(printf "%s-%s.md" "$Output_Filename_Prefix" "$1")
@@ -15,10 +40,11 @@ if [ -n "$1" ]; then
     fi
 else
     script_name=`basename $0`
-    echo "usage: $script_name [<tag_name>|/all]"
+    echo "usage: $script_name [<tag_name>|/all|/html]"
     echo
     echo -e "\t<tag_name>\tOutput specific tag commit history"
     echo -e "\t/all\t\tOutput all tag commit history"
+    echo -e "\t/html\t\tConvert markdown to html with style"
     exit
 fi
 
